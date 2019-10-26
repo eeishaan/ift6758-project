72a8afa6e69a7d11a12edca1cbfa85fa8adef6db
Ishaan Kumar <ffishaan@gmail.com>
2019-10-26 17:15:22 -0400

    add image data parsing

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -1,33 +1,111 @@
 import os
+
 import pandas as pd
+
 from .label_mappings import *
 
 
-def parse_text_data(param):
+def preprocess(df, scaler_type, scalers=None):
+    '''
+    df: DataFrame to be processed
+    scaler_type: sklearn.preprocessing scaler class
+    scalers: If supplied each column would be scaled
+        with the provided scaler
+    '''
+    used_scalers = {}
+    ret_df = df.copy(deep=True)
+    for c in df.columns:
+        if c in ['userId', 'faceID']:
+            continue
+        if scalers is None:
+            c_scaler = scaler_type()
+            used_scalers[c] = c_scaler
+            func = c_scaler.fit_transform
+        else:
+            c_scaler = scalers[c]
+            func = c_scaler.transform
+        ret_df[c] = func(df[[c]].values.astype(float))
+    if scalers is None:
+        return ret_df, used_scalers
+    return ret_df, scalers
+
+
+def parse_text_data(root):
     # TODO
     return None
 
 
-def parse_relational_data(param):
+def parse_relational_data(relation_file):
     # TODO
     return None
 
 
-def parse_image_data(param):
-    # TODO
-    return None
+def parse_image_data(image_file, profile_data, is_train):
+    image_data = pd.read_csv(image_file)
+    # don't filter for test data
+    if is_train:
+        image_data = image_data[image_data.userId.isin(profile_data.userid)]
+        image_data = image_data.dropna()
+
+    X = []
+    y = []
+    id_col = ['userId', 'faceID']
+    image_cols = image_data.columns
+    for row in profile_data.iterrows():
+        row = row[1]
+        faces = image_data[image_data.userId == row.userid]
+
+        # don't add to train data if features are unavailable
+        if faces.size == 0 and is_train:
+            continue
+
+        if faces.size == 0:
+            # Add a row full of None for test data so that we don't miss out some profiles
+            row_data = pd.DataFrame(
+                [[None]*len(image_cols)], columns=image_cols)
+        else:
+            # randomly choose the first row for train data when there are multiple faces
+            row_data = faces.iloc[0]
+
+        X.append(row_data.drop(labels=id_col))
+        if is_train:
+            y.append(row.gender)
+    if is_train:
+        return (X, y)
+    return (X,)
 
 
-def parse_input(input):
-    image_data = parse_image_data(os.path.join(input, 'Image'))
-    text_data = parse_text_data(os.path.join(input, 'Text'))
-    relational_data = parse_relational_data(os.path.join(input, 'Relation'))
-    profile_data = pd.read_csv(os.path.join(input, 'Profile/Profile.csv')).drop('Unnamed: 0', axis=1)
+def parse_input(root, is_train=True):
+    profile_data = pd.read_csv(os.path.join(
+        root, 'Profile/Profile.csv')).drop('Unnamed: 0', axis=1)
+
+    image_file = os.path.join(root, 'Image', 'profile.csv')
+    image_data = parse_image_data(image_file, profile_data, is_train)
+
+    text_root = parse_text_data(os.path.join(root, 'Text'))
+    text_data = parse_text_data(text_root)
+
+    relational_file = os.path.join(root, 'Relation', 'Relation.csv')
+    relational_data = parse_relational_data(relational_file)
 
     # Keep as dict and delegate to the submodels for dealing with different data sources
-    X = {"user_id": profile_data['userid'], "image": image_data, "relation": relational_data, "text": text_data}
+    X = {"user_id": profile_data['userid'], "image": image_data[0],
+         "relation": relational_data, "text": text_data}
 
-    return X, profile_data
+    if is_train:
+        # need to explicitly construct this as we don't have equal number of
+        # face data as profiles
+        y = {
+            'age': profile_data['age'].apply(lambda x: age_to_age_group(x)),
+            'gender': image_data[1],
+            'ope': profile_data['ope'],
+            'con': profile_data['con'],
+            'ext': profile_data['ext'],
+            'agr': profile_data['agr'],
+            'neu': profile_data['neu'],
+        }
+        return X, y
+    return X
 
 
 def parse_output(pred_df):
