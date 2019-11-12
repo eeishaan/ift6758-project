06d87d39dbaffb1e553c08c91f7a394126004793
Julien Horwood <julien@invivoai.com>
2019-11-12 14:39:05 -0500

    add pipeline with scalar to model

--- models/personality_estimators.py
+++ models/personality_estimators.py
@@ -1,11 +1,13 @@
 from sklearn.ensemble import RandomForestRegressor
 from models.final_estimator import BaseEstimator
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import make_pipeline
 from sklearn.externals import joblib
 
 class PersonalityTreeRegressor(BaseEstimator):
     def __init__(self, n_estimators=200):
         super(PersonalityTreeRegressor, self).__init__()
-        self.model = RandomForestRegressor(n_estimators=n_estimators)
+        self.model = make_pipeline(StandardScaler(), RandomForestRegressor(n_estimators=n_estimators))
     
     def fit(self, X, y):
         self.model.fit(X, y)
