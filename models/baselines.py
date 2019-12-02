ab0b7a8b0fd3532824e33b234649538259bac63a
Julien Horwood <julien@invivoai.com>
2019-12-01 23:08:30 -0500

    add try-catch in baselines

--- models/baselines.py
+++ models/baselines.py
@@ -6,9 +6,17 @@ class MajorityClassifier:
         self.clf = DummyClassifier(strategy='most_frequent')
 
     def fit(self, X, y):
+        try:
+            X = X['user_id']
+        except (IndexError, KeyError) as e:
+            pass
         self.clf.fit(X, y)
 
     def predict(self, X):
+        try:
+            X = X['user_id']
+        except (IndexError, KeyError) as e:
+            pass
         return self.clf.predict(X)
 
 
@@ -17,7 +25,15 @@ class MeanRegressor:
         self.reg = DummyRegressor(strategy='mean')
 
     def fit(self, X, y):
+        try:
+            X = X['user_id']
+        except (IndexError, KeyError) as e:
+            pass
         self.reg.fit(X, y)
 
     def predict(self, X):
+        try:
+            X = X['user_id']
+        except (IndexError, KeyError) as e:
+            pass
         return self.reg.predict(X)
