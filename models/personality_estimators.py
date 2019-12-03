3afe526dc0e0995901f59836eac02586effa03fc
Ishaan Kumar <ffishaan@gmail.com>
2019-12-02 19:29:22 -0500

    add multitask regressor for personality

--- models/personality_estimators.py
+++ models/personality_estimators.py
@@ -1,6 +1,7 @@
 from sklearn.decomposition import PCA
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.externals import joblib
+from sklearn.linear_model import MultiTaskElasticNetCV
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
 
@@ -19,6 +20,20 @@ class PersonalityTreeRegressor(BaseEstimator):
             self.model = make_pipeline(
                 StandardScaler(),
                 GradientBoostingRegressor(n_estimators=n_estimators))
+
+    def fit(self, X, y):
+        X = X['text']
+        self.model.fit(X, y)
+
+    def predict(self, X):
+        X = X['text']
+        return self.model.predict(X)
+
+
+class MultiTaskRegressor(BaseEstimator):
+    def __init__(self):
+        super(MultiTaskRegressor, self).__init__()
+        self.model = MultiTaskElasticNetCV(n_jobs=-1, cv=5)
 
     def fit(self, X, y):
         X = X['text']
