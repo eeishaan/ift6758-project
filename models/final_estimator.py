964c657f61aba610e5197ae0c9b402692cfb6742
Ishaan Kumar <ffishaan@gmail.com>
2019-12-02 19:30:09 -0500

    estimator for multitask personality regression

--- models/final_estimator.py
+++ models/final_estimator.py
@@ -69,7 +69,69 @@ class SingleTaskEstimator(BaseEstimator):
             'ext': personality_score(ypred['ext'], ytest['ext']),
             'agr': personality_score(ypred['agr'], ytest['agr']),
             'neu': personality_score(ypred['neu'], ytest['neu']),
-            'age': age_score(ypred['age'], ytest['age'],age_to_group),
+            'age': age_score(ypred['age'], ytest['age'], age_to_group),
+        }
+        if save:
+            pd.to_pickle(eval_results, 'evaluation_results.pkl')
+
+        print("The age accuracy is: {}\n \
+              The gender accuracy is: {}\n \
+              The ope rmse is: {}\n)\
+              The con rmse is: {}\n)\
+              The ext rmse is: {}\n)\
+              The agr rmse is: {}\n)\
+              The neu rmse is: {}\n".format(eval_results['age'],
+                                            eval_results['gender'],
+                                            eval_results['ope'],
+                                            eval_results['con'],
+                                            eval_results['ext'],
+                                            eval_results['agr'],
+                                            eval_results['neu'],))
+        return eval_results
+
+    def save(self, output_path):
+        joblib.dump(self, output_path)
+
+
+class MultiTaskEstimator(BaseEstimator):
+    def __init__(self, gender_clf, age_clf, per_reg):
+        # Independent task models
+        self.age_clf = age_clf
+        self.gender_clf = gender_clf
+        self.per_reg = per_reg
+        self.personality_keys = ['ope', 'con', 'ext', 'agr', 'neu']
+
+    def fit(self, X, y):
+        self.gender_clf.fit(X, y['gender'])
+        print("Trained gender classifier.")
+        self.age_clf.fit(X, y['age'])
+        print("Trained age classifier.")
+        self.per_reg.fit(X, y[self.personality_keys])
+        print("Trained personality regression.")
+
+    def predict(self, X):
+        pred_df = pd.DataFrame(index=X['user_id'], columns=[
+            'age', 'gender', 'ope', 'con', 'ext', 'agr', 'neu'])
+
+        pred_df['gender'] = self.gender_clf.predict(X)
+        pred_df['age'] = self.age_clf.predict(X)
+
+        personality_pred = self.per_reg.predict(X)
+        for key_idx, key in enumerate(self.personality_keys):
+            pred_df[key] = personality_pred[:, key_idx]
+
+        return pred_df
+
+    def eval(self, Xtest, ytest, save=False, age_to_group=True):
+        ypred = self.predict(Xtest)
+        eval_results = {
+            'gender': gender_score(ypred['gender'], ytest['gender']),
+            'ope': personality_score(ypred['ope'], ytest['ope']),
+            'con': personality_score(ypred['con'], ytest['con']),
+            'ext': personality_score(ypred['ext'], ytest['ext']),
+            'agr': personality_score(ypred['agr'], ytest['agr']),
+            'neu': personality_score(ypred['neu'], ytest['neu']),
+            'age': age_score(ypred['age'], ytest['age'], age_to_group),
         }
         if save:
             pd.to_pickle(eval_results, 'evaluation_results.pkl')
