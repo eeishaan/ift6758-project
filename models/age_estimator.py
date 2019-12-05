41b530e3cd0605c64427e52f729e0ba083108992
Vicki <vicki_anand@hotmail.com>
2019-12-05 13:23:15 -0500

    Add pep8 formatting for age _estimator.py using Black

--- models/age_estimator.py
+++ models/age_estimator.py
@@ -15,23 +15,38 @@ from models.final_estimator import BaseEstimator
 
 
 class AgeEstimator(BaseEstimator):
-    def __init__(self, n_estimators=1000, text_svd_components=50, image_svd_components=3, num_ensemble=3,
-                 gender_clf=None, ope_reg=None,
-                 con_reg=None, ext_reg=None, agr_reg=None, neu_reg=None, ):
+    def __init__(
+        self,
+        n_estimators=1000,
+        text_svd_components=50,
+        image_svd_components=3,
+        num_ensemble=3,
+        gender_clf=None,
+        ope_reg=None,
+        con_reg=None,
+        ext_reg=None,
+        agr_reg=None,
+        neu_reg=None,
+    ):
         super().__init__()
         # self.related_model = [gender_clf, ope_reg, con_reg, ext_reg, agr_reg, neu_reg]
         self.related_model = []
         estimators = []
         for i in range(num_ensemble):
             estimators.append(
-                (str(i), XGBClassifier(n_estimators=n_estimators, n_jobs=10, learning_rate=0.01, max_depth=3,
-                                       colsample_bytree=1)))
+                (
+                    str(i),
+                    XGBClassifier(
+                        n_estimators=n_estimators,
+                        n_jobs=10,
+                        learning_rate=0.01,
+                        max_depth=3,
+                        colsample_bytree=1,
+                    ),
+                )
+            )
 
-        self.clf = VotingClassifier(
-            estimators=estimators,
-            voting='hard',
-            n_jobs=1
-        )
+        self.clf = VotingClassifier(estimators=estimators, voting="hard", n_jobs=1)
         self.text_pca = TruncatedSVD(n_components=text_svd_components)
         self.image_pca = TruncatedSVD(n_components=image_svd_components)
         self.std_scaler = StandardScaler()
@@ -51,7 +66,9 @@ class AgeEstimator(BaseEstimator):
 
     def _normalize_data(self, data):
         scaled_features = self.std_scaler.fit_transform(data.values)
-        scaled_features_df = pd.DataFrame(scaled_features, index=data.index, columns=data.columns)
+        scaled_features_df = pd.DataFrame(
+            scaled_features, index=data.index, columns=data.columns
+        )
         return scaled_features_df
 
     def _preprocess(self, X, is_train_mode=False):
@@ -73,11 +90,14 @@ class AgeEstimator(BaseEstimator):
         # text_data = self.text_pca.transform(text_data)
         # image_data = self.image_pca.transform(image_data)
 
-        input_data = pd.concat([
-            pd.DataFrame(image_data.values),
-            pd.DataFrame(relation_data),
-            pd.DataFrame(text_data.values)
-        ], axis=1)
+        input_data = pd.concat(
+            [
+                pd.DataFrame(image_data.values),
+                pd.DataFrame(relation_data),
+                pd.DataFrame(text_data.values),
+            ],
+            axis=1,
+        )
         input_data = self.add_other_task_prediction(X, input_data)
         return input_data
 
@@ -93,7 +113,7 @@ class AgeEstimator(BaseEstimator):
         joblib.dump(self, output_path)
 
     def get_num_likes(self, relation_data):
-        relation_data_group = relation_data.groupby(['userId'])
+        relation_data_group = relation_data.groupby(["userId"])
         like_count_per_user = np.zeros(shape=(len(relation_data_group), 1))
         for i, (user_id, relation_data) in enumerate(relation_data_group):
             like_count_per_user[i] = len(relation_data["like_id"])
